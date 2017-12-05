fe33a15d19ea33030a14bf99bbf65863af63e7e5
Jason Cheung <jayson.h.cheung@gmail.com>
2017-12-05 01:49:38 -0800

    Basic User info view

--- MySteam/Models/PlayerModel.cs
+++ MySteam/Models/PlayerModel.cs
@@ -14,15 +14,19 @@ namespace MySteam.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string steamid { get; set; }
+        [Display(Name = "Visibility")]
         public UserVisibility communityvisibilitystate { get; set; }
         public int profilestate { get; set; }
+        [Display(Name = "Username")]
         public string personaname { get; set; }
         public int lastlogoff { get; set; }
         public string profileurl { get; set; }
         public string avatar { get; set; }
         public string avatarmedium { get; set; }
         public string avatarfull { get; set; }
-        public int personastate { get; set; }
+        [Display(Name = "State")]
+        public UserState personastate { get; set; }
+        [Display(Name = "Real Name")]
         public string realname { get; set; }
         public string primaryclanid { get; set; }
         public int timecreated { get; set; }
@@ -30,6 +34,14 @@ namespace MySteam.Models
         public string loccountrycode { get; set; }
         public string locstatecode { get; set; }
         public int loccityid { get; set; }
+
+        [NotMapped]
+        public string TimeSinceLastLogoff {
+            get {
+                var timespan = TimeSpan.FromSeconds(DateTimeOffset.Now.ToUnixTimeSeconds() - lastlogoff);
+                return timespan.ToString(@"h\:mm\:ss");
+            }
+        }
     }
 
     public class UserResponse
@@ -46,6 +58,17 @@ namespace MySteam.Models
     {
         Invisible = 1,  // private, friends, only, etc. 
         Visible = 3     // public
+    }
+
+    public enum UserState
+    {
+        Offline = 0,
+        Online = 1,
+        Busy = 2,
+        Away = 3,
+        Snooze = 4,
+        LookingToTrade = 5,
+        LookingToPlay = 6
     }
 
     #endregion
